910b2898f0315f5b9147b9fa8ddd6d779eafcc62
Rodrigo Viluron <119011125+RodriViluron@users.noreply.github.com>
2023-09-18 10:34:59 -0300

    Add files via upload

---
+++ HU-20) AltaModBajaRubrosPorductos/scriptHU-20.js
@@ -0,0 +1,30 @@
+// Obtener elementos
+const ventanaEmergente = document.getElementById("ventanaEmergente");
+const nuevoButton = document.querySelector(".contenedor-botones-botonNuevo");
+const editarIcon = document.querySelectorAll(".editar-icon");
+const cancelButton = document.getElementById("cancelar");
+const guardarButton = document.getElementById("guardar");
+
+// Mostrar la ventana emergente
+function mostrarventanaEmergente() {
+    ventanaEmergente.style.display = "block";
+}
+
+// Ocultar ventana emergente
+function ocultarventanaEmergente() {
+    ventanaEmergente.style.display = "none";
+}
+
+// Evento botón "Nuevo"
+nuevoButton.addEventListener("click", mostrarventanaEmergente);
+
+// Evento al hacer clic en un ícono "Editar"
+editarIcon.forEach((icon) => {
+    icon.addEventListener("click", mostrarventanaEmergente);
+});
+
+// Evento botón "Cancelar"
+cancelButton.addEventListener("click", ocultarventanaEmergente);
+
+// Evento botón "Guardar"
+
